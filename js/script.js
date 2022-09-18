af825d705d82560ec0be4332ca39010679d8f032
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-09-18 15:46:09 +0200

    nav bar scroll

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
-let navOffset=document.querySelector("header nav").offsetTop;
-console.log(navOffset);
+let header=document.querySelector("header")
+let nav=document.querySelector("header nav")
+let navOffset=nav.offsetTop;
+
+
+window.addEventListener('scroll' , (e) =>{
+    if(this.scrollY >= navOffset){
+        nav.classList.add("position-fixed","top-0","widthInherit");
+    }
+    else{
+        nav.classList.remove("position-fixed","top-0","widthInherit");
+    }
+    window.localStorage.setItem('scrollValue',this.scrollY);
+})
+
+
+window.addEventListener('load' , (e) =>{
+    let scrollValue= window.localStorage.getItem('scrollValue');
+    if(scrollValue >= navOffset){
+        nav.classList.add("position-fixed","top-0","widthInherit");
+    }
+})
